3acc64caf1d838a915329cc532983122c82ad719
Andriele Lange <andrielebarcelange@gmail.com>
2023-05-06 16:51:42 -0300

    vagao

--- src/Vagao.java
+++ src/Vagao.java
@@ -1,3 +1,40 @@
 public class Vagao{
-    //teste
+    private int identificador; 
+    private String descricao;
+    private double capacidadeCarga;
+    private static int proxIdent = 1;
+
+    public Vagao(String descricao, double capacidadeCarga) {
+        this.identificador = proxIdent;
+        this.descricao = descricao;
+        this.capacidadeCarga = capacidadeCarga;
+        proxIdent = proxIdent + 1;
+    }
+
+    public int getIdentificador() {
+        return identificador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getCapacidadeCarga() {
+        return capacidadeCarga;
+    }
+
+    @Override
+    public String toString() {
+        return "\n Vagao\n identificador=" + identificador + "\n descricao=" + descricao + "\n capacidadeCarga="
+                + capacidadeCarga;
+    }
+
+    /**
+     * public void setTrem(int id){
+     * this.trem = id;
+     * }
+     */
+
+     
+
 }
